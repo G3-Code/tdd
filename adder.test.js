74d910c6ad3ff9b282ec9207d5a5a835949df9cf
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-05-02 08:39:48 -0500

    added the third test case

--- adder.test.js
+++ adder.test.js
@@ -7,4 +7,7 @@ describe("adder", () => {
   it("should add just one number", () => {
     expect(adder(4)).toBe(4);
   });
+  it("should add three numbers", () => {
+    expect(adder(1, 2, 3)).toBe(6);
+  });
 });
